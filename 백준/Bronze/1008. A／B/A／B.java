4abac5fb6fc18d08cff7ca73d6600dd2b55bf7a9
leeeunjae1 <128332821+leeeunjae1@users.noreply.github.com>
2024-09-23 06:21:15 +0900

    [Bronze V] Title: A/B, Time: 180 ms, Memory: 17956 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        
+        double x = in.nextDouble();
+        double y = in.nextDouble();
+        
+        System.out.print(x/y);
+        
+    }
+}
