39b8b8cb05d4ce26e198778e0f20dba17c1afd02
leeeunjae1 <128332821+leeeunjae1@users.noreply.github.com>
2024-09-23 06:24:17 +0900

    [Bronze V] Title: 1998년생인 내가 태국에서는 2541년생?!, Time: 168 ms, Memory: 17776 KB -BaekjoonHub

---
+++ 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args) {
+       
+        Scanner in = new Scanner(System.in);
+        
+        int x = in.nextInt();
+        
+        int indiayear = 543;
+        
+        int y = x - indiayear;
+        
+        System.out.print(y);
+    }
+}
