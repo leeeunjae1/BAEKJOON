e3aab33752315489f73cead837139409a8d65fb1
leeeunjae1 <128332821+leeeunjae1@users.noreply.github.com>
2024-09-23 06:23:46 +0900

    [Bronze V] Title: ??!, Time: 180 ms, Memory: 17668 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args) {
+       
+        Scanner in = new Scanner(System.in);
+        
+        String x = in.next();
+        
+        String lastString = "??!";
+        
+        System.out.print(x + lastString);
+        
+    }
+}
