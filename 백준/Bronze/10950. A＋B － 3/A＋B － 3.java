5d65f738fa51fa8c21ae5fc24673f4a74e41cb47
leeeunjae1 <128332821+leeeunjae1@users.noreply.github.com>
2024-09-23 10:59:52 +0900

    [Bronze V] Title: A+B - 3, Time: 192 ms, Memory: 18084 KB -BaekjoonHub

---
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+    
+		Scanner in = new Scanner(System.in);
+		
+		int x = in.nextInt();
+		
+		for(int i = 1; i <= x; i++) {
+			
+			int a = in.nextInt();
+			int b = in.nextInt();
+			
+			System.out.printf("%d\n", a + b);			
+			
+		}
+		
+	}
+}
