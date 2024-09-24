074a3fe74b6cdb615533d567ba20c8fb67fe83cb
leeeunjae1 <128332821+leeeunjae1@users.noreply.github.com>
2024-09-24 13:01:18 +0900

    [Bronze IV] Title: 숫자의 합, Time: 176 ms, Memory: 17744 KB -BaekjoonHub

---
+++ 백준/Bronze/11720. 숫자의 합/숫자의 합.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String args[]) {
+        
+        Scanner in = new Scanner(System.in);
+        
+        int N = in.nextInt();
+        
+        String a = in.next();
+        
+        int sum = 0;
+        
+        for(int i = 0; i < N; i++) {
+        	sum = sum + a.charAt(i) - '0';
+        }
+        
+        System.out.print(sum);
+    	
+    }
+}
