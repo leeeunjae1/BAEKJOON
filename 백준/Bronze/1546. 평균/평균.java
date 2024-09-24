94c3d64e5c4e1a604242b33c17f137d85470fbdc
leeeunjae1 <128332821+leeeunjae1@users.noreply.github.com>
2024-09-24 09:25:11 +0900

    [Bronze I] Title: 평균, Time: 112 ms, Memory: 14448 KB -BaekjoonHub

---
+++ 백준/Bronze/1546. 평균/평균.java
@@ -0,0 +1,50 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String args[]) throws IOException {
+        
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        int N = Integer.parseInt(br.readLine());
+        
+        double arr[] = new double [N];
+        
+        double max = Double.MIN_VALUE;
+        
+        double sum = 0;
+        
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        
+        for(int i = 0; i < N; i++) {
+        	arr[i] = Float.parseFloat(st.nextToken());		
+
+        }
+        
+    	for(int j = 0; j < arr.length; j++) {
+        	
+    		if(arr[j] > max) {
+    			max = arr[j];
+    		}
+    	}
+    	
+    	for(int k = 0; k < arr.length; k++) {
+    		arr[k] = arr[k] / max * 100;
+    		
+    		sum = sum + arr[k];
+    		
+    	}
+    	
+    	bw.write(String.valueOf(sum/N));
+    	
+    	br.close();
+    	bw.flush();
+    	bw.close();
+    	
+    }
+}
