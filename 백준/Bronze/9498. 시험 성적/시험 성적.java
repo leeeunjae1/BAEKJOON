422455c619d648e6ad4bd424f433cf4b79888979
leeeunjae1 <128332821+leeeunjae1@users.noreply.github.com>
2024-11-07 08:06:18 +0900

    [Bronze V] Title: 시험 성적, Time: 108 ms, Memory: 14224 KB -BaekjoonHub

--- 백준/Bronze/9498. 시험 성적/시험 성적.java
+++ 백준/Bronze/9498. 시험 성적/시험 성적.java
@@ -1,23 +1,28 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
-public class Main {
-	public static void main(String[] args) {
-    
-		Scanner in = new Scanner(System.in);
+public class Main{
+	public static void main(String [] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
-		int x = in.nextInt();
+		String str = br.readLine();
+		StringTokenizer st = new StringTokenizer(str, " ");
 		
-		if(90 <= x && x <=100) {
-			System.out.print("A");
-		} else if (80 <= x && x < 90) {
-			System.out.print("B");
-		} else if (70 <= x && x < 80){
-			System.out.print("C");
-		} else if (60 <= x && x < 70) {
-			System.out.print("D");
+		int score = Integer.parseInt(st.nextToken());
+		
+		if (90 <= score && score <= 100) {
+			System.out.println("A");
+		} else if (80 <= score && score < 90) {
+			System.out.println("B");
+		} else if (70 <= score && score < 80) {
+			System.out.println("C");
+		} else if (60 <= score && score < 70) {
+			System.out.println("D");
 		} else {
-			System.out.print("F");
+			System.out.println("F");
 		}
- 
+		
 	}
 }
