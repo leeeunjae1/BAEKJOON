004266b1fa922a5ce7a513f0b768a9edde1475a7
leeeunjae1 <128332821+leeeunjae1@users.noreply.github.com>
2024-11-07 10:47:06 +0900

    [Bronze IV] Title: 영수증, Time: 100 ms, Memory: 14096 KB -BaekjoonHub

--- 백준/Bronze/25304. 영수증/영수증.java
+++ 백준/Bronze/25304. 영수증/영수증.java
@@ -1,32 +1,33 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
-public class Main {
-	public static void main(String[] args) {
-    
-		Scanner in = new Scanner(System.in);
+public class Main{
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
-		int sum = in.nextInt();
+		int sum = Integer.parseInt(br.readLine());
 		
-		int product = in.nextInt();
+		int count = Integer.parseInt(br.readLine());
 		
 		int cal = 0;
 		
-		
-		for(int i = 1; i <= product; i++) {
+		for(int i = 0; i < count; i++) {
+			String str = br.readLine();
+			StringTokenizer st = new StringTokenizer(str, " ");
 			
-			int price = in.nextInt();
+			int price = Integer.parseInt(st.nextToken());
+			int count1 = Integer.parseInt(st.nextToken());
 			
-			int count = in.nextInt();
-			
-			cal = cal + (price * count);
+			cal = cal + (price * count1);
 			
 		}
 		
-		if(sum == cal) {
+		if(cal == sum) {
 			System.out.println("Yes");
 		} else {
 			System.out.println("No");
 		}
-		
 	}
 }
