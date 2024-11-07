f053ec391adceb93f3a73c8cb7083a5c2825e498
leeeunjae1 <128332821+leeeunjae1@users.noreply.github.com>
2024-11-07 13:18:48 +0900

    [Bronze III] Title: 최소, 최대, Time: 484 ms, Memory: 92064 KB -BaekjoonHub

--- 백준/Bronze/10818. 최소， 최대/최소， 최대.java
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.java
@@ -4,39 +4,41 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
- 
-public class Main {
-	public static void main(String args[]) throws IOException{
-		
-		BufferedReader br = new BufferedReader(new  InputStreamReader(System.in));
+
+public class Main{
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		
-		int N = Integer.parseInt(br.readLine());
+		int max = -1000000;
+		int min = 1000000;
 		
-		int arr[] = new int[N];
+		int A = Integer.parseInt(br.readLine());
 		
-		int max = Integer.MIN_VALUE;;
+		int arr[] = new int[A];
 		
-		int min = Integer.MAX_VALUE;;
-		
-		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		String str = br.readLine();
+		StringTokenizer st = new StringTokenizer(str, " ");
 		
 		for(int i = 0; i < arr.length; i++) {
-			arr[i] = Integer.parseInt(st.nextToken());
+			int a = Integer.parseInt(st.nextToken());
+			
+			arr[i] = a;
 		}
 		
-		for(int j = 0; j < arr.length; j++) {
+		for(int j = 0; j <arr.length; j++) {
 			if(arr[j] > max) {
 				max = arr[j];
 			}
-			if (arr[j] < min) {
-				min = arr[j];
+		}
+		
+		for(int k = 0; k < arr.length; k++) {
+			if (arr[k] < min) {
+				min = arr[k];
 			}
 		}
 		
 		bw.write(min + " " + max);
-		
-		br.close();
 		bw.flush();
 		bw.close();
 		
