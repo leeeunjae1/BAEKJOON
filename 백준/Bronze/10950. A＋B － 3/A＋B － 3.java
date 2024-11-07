ffe3d15ace77f68ccd7ce5061b7bb21c614df46a
leeeunjae1 <128332821+leeeunjae1@users.noreply.github.com>
2024-11-07 10:36:21 +0900

    [Bronze V] Title: A+B - 3, Time: 120 ms, Memory: 15836 KB -BaekjoonHub

--- 백준/Bronze/10950. A＋B － 3/A＋B － 3.java
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.java
@@ -1,20 +1,28 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
 
-public class Main {
-	public static void main(String[] args) {
-    
-		Scanner in = new Scanner(System.in);
+public class Main{
+	public static void main(String[] args)throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		
-		int x = in.nextInt();
+		int A = Integer.parseInt(br.readLine());
 		
-		for(int i = 1; i <= x; i++) {
+		for(int i = 0; i < A; i++) {
+			String str = br.readLine();
+			StringTokenizer st = new StringTokenizer(str, " ");
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
 			
-			int a = in.nextInt();
-			int b = in.nextInt();
-			
-			System.out.printf("%d\n", a + b);			
+			bw.write((a+b) + "\n");
 			
 		}
 		
+		bw.flush();
+		bw.close();
 	}
 }
