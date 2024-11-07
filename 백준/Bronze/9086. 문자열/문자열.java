d224ab37140a027ae22f03f83205219663e67a74
leeeunjae1 <128332821+leeeunjae1@users.noreply.github.com>
2024-11-07 14:50:12 +0900

    [Bronze V] Title: 문자열, Time: 108 ms, Memory: 14232 KB -BaekjoonHub

--- 백준/Bronze/9086. 문자열/문자열.java
+++ 백준/Bronze/9086. 문자열/문자열.java
@@ -1,27 +1,18 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 
-public class Main {
-    public static void main(String args[]) throws IOException {
-        
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        
-        int N = Integer.parseInt(br.readLine());
-        
-        for(int i = 0; i < N; i++) {
-        	String S = br.readLine();
-        	char A = S.charAt(0);
-        	char B = S.charAt(S.length()-1);
-        	bw.write(A + "" + B + "\n");
-        }
-        
-        br.close();
-        bw.flush();
-        bw.close();
-
-    }
+public class Main{
+	public static void main(String [] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int a = Integer.parseInt(br.readLine());
+		
+		for(int i = 0; i < a; i++) {
+			String A = br.readLine();
+			System.out.printf("%c%c\n", A.charAt(0), A.charAt(A.length() - 1));
+		}
+		
+	}
 }
