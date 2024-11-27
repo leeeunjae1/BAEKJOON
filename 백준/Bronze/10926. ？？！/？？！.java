db03cf82b79d03c71854f93aa24d5ef035ecaec4
leeeunjae1 <128332821+leeeunjae1@users.noreply.github.com>
2024-11-28 04:01:43 +0900

    [Bronze V] Title: ??!, Time: 108 ms, Memory: 14260 KB -BaekjoonHub

--- 백준/Bronze/10926. ？？！/？？！.java
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -1,15 +1,14 @@
-import java.util.Scanner;
+import java.util.*;
+import java.io.*;
 
-public class Main{
-    public static void main(String[] args) {
-       
-        Scanner in = new Scanner(System.in);
-        
-        String x = in.next();
-        
-        String lastString = "??!";
-        
-        System.out.print(x + lastString);
-        
-    }
+public class Main {
+	public static void main(String[] args) throws IOException{
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		String A = br.readLine();
+		
+		System.out.println(A + "??!");
+		
+	}
 }
