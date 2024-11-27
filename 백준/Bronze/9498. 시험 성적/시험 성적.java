29207216bb2b791a8893c080835a09337100d195
leeeunjae1 <128332821+leeeunjae1@users.noreply.github.com>
2024-11-28 04:38:18 +0900

    [Bronze V] Title: 시험 성적, Time: 108 ms, Memory: 14200 KB -BaekjoonHub

--- 백준/Bronze/9498. 시험 성적/시험 성적.java
+++ 백준/Bronze/9498. 시험 성적/시험 성적.java
@@ -1,28 +1,22 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.*;
+import java.io.*;
 
 public class Main{
-	public static void main(String [] args) throws IOException{
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
-		String str = br.readLine();
-		StringTokenizer st = new StringTokenizer(str, " ");
+		int A = Integer.parseInt(br.readLine());
 		
-		int score = Integer.parseInt(st.nextToken());
-		
-		if (90 <= score && score <= 100) {
+		if(A >= 90) {
 			System.out.println("A");
-		} else if (80 <= score && score < 90) {
+		} else if (A >= 80) {
 			System.out.println("B");
-		} else if (70 <= score && score < 80) {
+		} else if (A >= 70) {
 			System.out.println("C");
-		} else if (60 <= score && score < 70) {
+		} else if (A >= 60) {
 			System.out.println("D");
 		} else {
 			System.out.println("F");
 		}
-		
 	}
 }
