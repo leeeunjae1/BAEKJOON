a48de427704e6d7b2713e2d9b4741379acf20218
leeeunjae1 <128332821+leeeunjae1@users.noreply.github.com>
2024-11-29 23:06:09 +0900

    [level 0] Title: 문자열 곱하기, Time: 2.40 ms, Memory: 94.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181940. 문자열 곱하기/문자열 곱하기.java
@@ -0,0 +1,9 @@
+class Solution {
+    public String solution(String my_string, int k) {
+        String result = "";
+        for(int i = 0; i < k; i++) {
+            result = result + my_string;
+        }
+        return result;
+    }
+}
