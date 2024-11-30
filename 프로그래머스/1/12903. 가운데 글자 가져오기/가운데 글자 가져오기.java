cb23bc590ecee9097609c9a3dc5ac0f9ba19793d
leeeunjae1 <128332821+leeeunjae1@users.noreply.github.com>
2024-11-30 13:38:38 +0900

    [level 1] Title: 가운데 글자 가져오기, Time: 0.02 ms, Memory: 75.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.java
@@ -0,0 +1,12 @@
+class Solution {
+    public String solution(String s) {
+        int length = s.length();
+        int middle = length / 2;
+
+        if (length % 2 == 0) {
+            return s.substring(middle - 1, middle + 1);
+        } else {
+            return s.substring(middle, middle + 1);
+        }
+    }
+}
