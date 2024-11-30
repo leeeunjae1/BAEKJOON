705b5ba969cc95bdd37736e350c268200e06e273
leeeunjae1 <128332821+leeeunjae1@users.noreply.github.com>
2024-11-30 15:24:36 +0900

    [level 1] Title: 예산, Time: 0.54 ms, Memory: 82.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/12982. 예산/예산.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class Solution {
+    public int solution(int[] d, int budget) {
+        Arrays.sort(d);
+        
+        int count = 0;
+        for (int i = 0; i < d.length; i++) {
+            if (budget >= d[i]) {
+                budget -= d[i];
+                count++;
+            } else {
+                break;
+            }
+        }
+        
+        return count;
+    }
+}
